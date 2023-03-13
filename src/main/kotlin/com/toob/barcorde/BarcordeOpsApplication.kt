89bd16e4da394baca9904eba0e93a2aa349deef8
Thabo Matjuda <thabo@anylytical.co.za>
2023-03-13 14:53:30 +0200

    Initial Push

---
+++ src/main/kotlin/com/toob/barcorde/BarcordeOpsApplication.kt
@@ -0,0 +1,11 @@
+package com.toob.barcorde
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BarcordeOpsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BarcordeOpsApplication>(*args)
+}
